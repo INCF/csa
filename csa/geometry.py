94bfe8bec24eb2e76f6cd2cfcddc4b250b521add
Patrick Herbers <patrick.herbers@rub.de>
2017-09-28 17:06:14 +0200

    Added grid3d and random3d to geometry functions
    The grid3d and random3d mirror the functionality of grid2d and random2d.
    `random3d` and the added `euclidDistance3d` and `euclidMetric3d` use numpy arrays for increased performance.
    Performance for the 3D functions is slightly lower than 2D functions, but scales similarly.

--- csa/geometry.py
+++ csa/geometry.py
@@ -81,3 +81,91 @@ def euclidToroidDistance2d (p1, p2, xScale=1.0, yScale=1.0):
 def euclidToroidMetric2d (g1, g2 = None, xScale=1.0, yScale=1.0):
     g2 = g1 if g2 == None else g2
     return lambda i, j: euclidToroidDistance2d (g1 (i), g2 (j), xScale, yScale)
+
+# 3D functions
+
+def grid3d(width, xScale = 1.0, yScale = 1.0, zScale = 1.0, x0 = 0.0, y0 = 0.0, z0 = 0.0):
+    """Returns a 3D grid between (0, 0, 0) and (1, 1, 1)
+    :param width: The number of rows/columns the grid has
+    :type width: int
+    :param xScale: Scales the grid along the x axis
+    :type xScale: float
+    :param yScale: Scales the grid along the y axis
+    :type yScale: float
+    :param zScale: Scales the grid along the z axis
+    :type zScale: float
+    :param x0: Translates the grid along the x axis
+    :type xScale: float
+    :param y0: Translates the grid along the y axis
+    :type yScale: float
+    :param z0: Translates the grid along the z axis
+    :type zScale: float
+    :return: A callable grid that returns 3d positions when given an index"""
+    xScale /= width
+    yScale /= width
+    zScale /= width
+    g = lambda i: \
+        (x0 + xScale * (i % width), y0 + yScale * ((i % (width*width)) / width), z0 + zScale * (i / (width*width)))
+    g.type = 'grid3d'
+    g.width = width
+    g.xScale = xScale
+    g.yScale = yScale
+    g.zScale = zScale
+    g.x0 = x0
+    g.y0 = y0
+    g.z0 = z0
+    g.inverse = lambda x, y, z: \
+                    int (round (x / xScale - x0)) \
+                    + width * (int (round (y / yScale - y0)
+                    + width * int (round (z / zScale - z0))))
+    return g
+    
+def random3d(N, xScale = 1.0, yScale = 1.0, zScale = 1.0):
+    """Creates a set of points scattered uniformly inside a 3D box
+    :param N: Number of 3D points
+    :type N: int
+    :param xScale: The scale of the box on the x axis
+    :type xScale: float
+    :param yScale: The scale of the box on the y axis
+    :type yScale: float
+    :param zScale: The scale of the box on the z axis
+    :type zScale: float
+    """
+    coords = _numpy.random.random((N, 3))
+    coords[...,0] *= xScale
+    coords[...,1] *= yScale
+    coords[...,2] *= zScale
+    g = lambda i: coords[i]
+    g.type = 'random'
+    g.N = N
+    g.xScale = xScale
+    g.yScale = yScale
+    g.zScale = zScale
+    g.inverse = lambda x, y, z, domain=_iset.IntervalSet ((0, N - 1)): \
+                    _numpy.array ([euclidDistance3d (_numpy.array((x, y, z)), g(i)) \
+                                   for i in domain]).argmin () \
+                                   + domain.min ()
+    return g
+
+def euclidDistance3d(p1, p2):
+    """Returns the euclidean distance in 3D between two points
+    :param p1: The first point
+    :type p1: numpy.array((3))
+    :param p2: The second point
+    :type p2: numpy.array((3))
+    :return: The euclidean distance
+    :rtype: float
+    """
+    return _numpy.linalg.norm(p2 - p1)
+
+def euclidMetric3d (g1, g2 = None):
+    """Returns an euclidean metric for 3D points
+    :param g1: The first group of points
+    :type g1: callable
+    :param g2: The second group of points. If None, the first group of points is used
+    :type g2: callable
+    :return: A 3D euclidean metric function
+    :rtype: function
+    """
+    g2 = g1 if g2 == None else g2
+    return lambda i, j: euclidDistance3d (g1 (i), g2 (j))
